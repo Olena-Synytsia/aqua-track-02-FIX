2e2bcbe7f2bd8d91c48a138a51650758d2e2abfc
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2025-01-08 16:35:52 +0200

    snrc

---
+++ src/components/WaterMainInfo/WaterMainInfo.jsx
@@ -0,0 +1,14 @@
+import { Text, StyleSheet, View } from "react-native";
+import React, { Component } from "react";
+
+export default class WaterMainInfo extends Component {
+  render() {
+    return (
+      <View>
+        <Text>WaterMainInfo</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
