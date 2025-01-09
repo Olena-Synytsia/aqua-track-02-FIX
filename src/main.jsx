e9ad92e33c776f53bfea3caacdf1fbea9916483b
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2025-01-09 16:52:49 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import { persistor, store } from "./redux/store.js";
+// import { persistor, store } from "./redux/store.js";
 import { BrowserRouter } from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
+// import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
