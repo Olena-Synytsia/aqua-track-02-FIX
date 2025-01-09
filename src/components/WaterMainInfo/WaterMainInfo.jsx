b20753afbe54bbea9542215bbcebb608ad5f3804
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2025-01-09 13:10:55 +0200

    add image

--- src/components/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/WaterMainInfo/WaterMainInfo.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import AddWaterBtn from "./AddWaterBtn/AddWaterBtn.jsx";
+import WaterDailyNorma from "./WaterDailyNorma/WaterDailyNorma.jsx";
+import WaterProgressBar from "./WaterProgressBar/WaterProgressBar.jsx";
 
 const WaterMainInfo = () => {
   return <div>WaterMainInfo</div>;
