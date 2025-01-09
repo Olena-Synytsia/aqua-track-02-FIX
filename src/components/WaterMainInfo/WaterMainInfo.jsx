bad38df437f13230309f03c0bbc1a2db976cd424
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2025-01-09 15:58:19 +0200

    WaterMainInfo

--- src/components/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/WaterMainInfo/WaterMainInfo.jsx
@@ -1,10 +1,28 @@
-import React from "react";
 import AddWaterBtn from "./AddWaterBtn/AddWaterBtn.jsx";
 import WaterDailyNorma from "./WaterDailyNorma/WaterDailyNorma.jsx";
 import WaterProgressBar from "./WaterProgressBar/WaterProgressBar.jsx";
+import css from "./WaterMainInfo.module.css";
+import { default as bottel1x } from "../../shared/images/trackerPage/bottel@x1.webp";
+import { default as bottel2x } from "../../shared/images/trackerPage/bottel@x.webp";
+import Logo from "../../shared/components/Logo/Logo.jsx";
 
 const WaterMainInfo = () => {
-  return <div>WaterMainInfo</div>;
+  return (
+    <div className={css.waterMainInfo}>
+      <Logo className={css.logoTracker} />
+      <WaterDailyNorma />
+      <WaterProgressBar />
+      <AddWaterBtn />
+      <div className={css.waterContainer}>
+        <img
+          className={css.image}
+          src={bottel1x}
+          srcSet={`${bottel1x} 1x, ${bottel2x} 2x`}
+          alt="Water bottel image"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default WaterMainInfo;
