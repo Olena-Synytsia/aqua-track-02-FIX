cbcc300d3aa2c6e98a430b05518b41263d0854ac
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2025-01-10 20:27:12 +0200

    fixed-month

--- src/components/TrackerPage/WaterDailedInfo/MonthInfo/CalendarPagination.jsx
+++ src/components/TrackerPage/WaterDailedInfo/MonthInfo/CalendarPagination.jsx
@@ -1,28 +1,27 @@
-import React from "react";
+// import React from "react";
 import dayjs from "dayjs";
-import s from "./CalendarPagination.module.css"
+import s from "./CalendarPagination.module.css";
 
 const CalendarPagination = ({ selectedDate, onDateChange }) => {
-    const handlePreviousMonth = () => {
-      onDateChange(dayjs(selectedDate).subtract(1, "month").toDate());
-    };
-  
-    const handleNextMonth = () => {
-      onDateChange(dayjs(selectedDate).add(1, "month").toDate());
-    };
-  
-    return (
-       
-      <div className={s.calendar-pagination}>
-         <h1>Month</h1>
-        <button onClick={handlePreviousMonth}>{"<"}</button>
-        <span>{dayjs(selectedDate).format("MMMM, YYYY")}</span>
-        <button onClick={handleNextMonth}>{">"}</button>
-        <svg className={s.icon-pie}>
+  const handlePreviousMonth = () => {
+    onDateChange(dayjs(selectedDate).subtract(1, "month").toDate());
+  };
+
+  const handleNextMonth = () => {
+    onDateChange(dayjs(selectedDate).add(1, "month").toDate());
+  };
+
+  return (
+    <div className={s.calendarpagination}>
+      <h1>Month</h1>
+      <button onClick={handlePreviousMonth}>{"<"}</button>
+      <span>{dayjs(selectedDate).format("MMMM, YYYY")}</span>
+      <button onClick={handleNextMonth}>{">"}</button>
+      <svg className={s.iconpie}>
         <use href="src/assets/sprite.svg#icon-pie-chart"></use>
       </svg>
-      </div>
-    );
-  };
-  
-  export default CalendarPagination;
+    </div>
+  );
+};
+
+export default CalendarPagination;
