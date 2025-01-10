7b14c8f16a9116bc068b66f5714ab745e50d17a0
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2025-01-10 10:24:15 +0200

    selectors

--- src/components/WaterMainInfo/WaterProgressBar/WaterProgressBar.jsx
+++ src/components/WaterMainInfo/WaterProgressBar/WaterProgressBar.jsx
@@ -1,7 +1,92 @@
-// import React from "react";
-import { Box, Slider } from "@mui/material";
+import { useEffect } from "react";
+import { Box } from "@mui/material/Box";
+import { Slider } from "@mui/material/Slider";
+import { styled } from "@mui/material/styled";
+import { Tooltip, tooltipClasses } from "@mui/material/Tooltip";
+import { useDispatch, useSelector } from "react-redux";
+import { apiGetWaterDay } from "../../../redux/actions/water";
+import { selectDate, selectPercentDay } from "../../../redux/selectors";
+import css from "./WaterProgressBar.module.css";
+import { useTranslation } from "react-i18next";
+
+const CustomTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: "#323f47",
+    color: "#ffffff",
+    fontSize: "12px",
+    borderRadius: "8px",
+  },
+  [`& .${tooltipClasses.arrow}`]: {
+    color: "#323f47",
+  },
+});
+
+function ValueLabelComponent(props) {
+  const { children, value, open } = props;
+  return (
+    <CustomTooltip
+      open={open}
+      enterTouchDelay={0}
+      placement="top"
+      title={`${Math.min(value, 100).toFixed(0)}%`}
+      arrow
+    >
+      {children}
+    </CustomTooltip>
+  );
+}
 
 const WaterProgressBar = () => {
+  const { t } = useTranslation();
+  const selectedDate = useSelector(selectDate);
+  const percentDay = useSelector(selectPercentDay);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(apiGetWaterDay(selectedDate));
+  }, [selectedDate, dispatch]);
+
+  const percent = percentDay || 0;
+
+  const isToday = (someDate) => {
+    const today = new Date();
+    return (
+      someDate.getDate() === today.getDate() &&
+      someDate.getMonth() === today.getMonth() &&
+      someDate.getFullYear() === today.getFullYear()
+    );
+  };
+
+  const months = {
+    january: t("ChooseDate.january"),
+    february: t("ChooseDate.february"),
+    march: t("ChooseDate.march"),
+    april: t("ChooseDate.april"),
+    may: t("ChooseDate.may"),
+    june: t("ChooseDate.june"),
+    july: t("ChooseDate.july"),
+    august: t("ChooseDate.august"),
+    september: t("ChooseDate.september"),
+    october: t("ChooseDate.october"),
+    november: t("ChooseDate.november"),
+    december: t("ChooseDate.december"),
+  };
+
+  const formatDate = (date) => {
+    const dateObj = new Date(date);
+    if (isToday(dateObj)) {
+      return t("waterMainInfo.today");
+    } else {
+      const day = dateObj.getDate();
+      const month = dateObj
+        .toLocaleString("en-US", { month: "long" })
+        .toLowerCase();
+      return `${day}, ${months[month]}`;
+    }
+  };
+
   return (
     <div className={css.container} data-tour="step-3">
       <div className={css.title}>{formatDate(selectedDate)}</div>
