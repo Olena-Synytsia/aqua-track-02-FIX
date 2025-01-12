2364ab003e62c50df5bdf21f78bb9e72c251338c
Roman Zhelnov <roman95zhelnov@gmail.com>
2025-01-12 16:29:56 +0200

    add water Form

--- src/components/Modal/WaterForm/WaterForm.jsx
+++ src/components/Modal/WaterForm/WaterForm.jsx
@@ -1,4 +1,23 @@
-const WaterForm = () => {
-  return <div></div>;
+import { RxMinusCircled, RxPlusCircled } from "react-icons/rx";
+import s from "./WaterForm.module.css";
+
+const WaterForm = ({ operationType, value = "50ml" }) => {
+  const title =
+    operationType === "add" ? "Choose a value" : "Correct entered data:";
+  return (
+    <form>
+      <h2>{title}</h2>
+      <p>Amount of water:</p>
+      <div className={s.counter}>
+        <button className={s.counterBtn}>
+          <RxMinusCircled className={s.iconBtn} />
+        </button>
+        <p className={s.counterValue}>{value}</p>
+        <button className={s.counterBtn}>
+          <RxPlusCircled className={s.iconBtn} />
+        </button>
+      </div>
+    </form>
+  );
 };
 export default WaterForm;
