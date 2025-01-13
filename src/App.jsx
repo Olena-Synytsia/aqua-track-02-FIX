dd2ccae57233d3001d4293c1aa27b205e646091a
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2025-01-13 16:12:21 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Route, Routes } from "react-router-dom";
 import WelcomePage from "./pages/WelcomePage/WelcomePage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
-import TrackerPage from "./pages/TrackerPage/TrackerPage.jsx";
+import TrackerPage from "./src/pages/TrackerPage/TrackerPage";
 import SharedLayout from "./components/SharedLayout";
 import { PrivateRoute } from "./components/PrivateRoute";
 import { RestrictedRoute } from "./components/RestrictedRoute";
