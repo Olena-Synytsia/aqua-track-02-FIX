156a02dc9fe338608ba72585856c252841d1e943
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2025-01-13 02:10:55 +0200

    Update WaterMainInfo.jsx

--- src/components/TrackerPage/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/TrackerPage/WaterMainInfo/WaterMainInfo.jsx
@@ -1,11 +1,11 @@
 import AddWaterBtn from "./AddWaterBtn/AddWaterBtn";
 import WaterDailyNorma from "./WaterDailyNorma/WaterDailyNorma";
 import WaterProgressBar from "./WaterProgressBar/WaterProgressBar";
-
+import "aos/dist/aos.css";
 import css from "./WaterMainInfo.module.css";
 
-import { bottel1x } from "../../../assets/img/index.js";
-import { bottel2x } from "../../../assets/img/index.js";
+import { default as bottel1x } from "../../../assets/img/bottel@x1.webp";
+import { default as bottel2x } from "../../../assets/img/bottel@x2.webp";
 import Logo from "../../HomePage/WelcomeSection/Logo/Logo";
 
 const WaterMainInfo = () => {
