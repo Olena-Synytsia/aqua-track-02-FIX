c852213b488abeb2e43f6988b23a0b6e054c0796
Ваше ім'я <constantin161089@gmail.com>
2025-01-14 19:03:59 +0200

    tracker page!!!

--- src/components/TrackerPage/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/TrackerPage/WaterMainInfo/WaterMainInfo.jsx
@@ -1,12 +1,11 @@
-import AddWaterBtn from "./AddWaterBtn/AddWaterBtn.jsx";
-import WaterDailyNorma from "./WaterDailyNorma/WaterDailyNorma.jsx";
-import WaterProgressBar from "./WaterProgressBar/WaterProgressBar.jsx";
-
 import css from "./WaterMainInfo.module.css";
 
 import bottleDescX1 from "../../../assets/img/bottle-desc-x1.jpg";
 import bottleDescX2 from "../../../assets/img/bottle-desc-x2.jpg";
-import Logo from "../../HomePage/WelcomeSection/Logo/Logo.jsx";
+import WaterDailyNorma from "./WaterDailyNorma/WaterDailyNorma";
+import AddWaterBtn from "../WaterDailedInfo/DailyInfo/AddWaterBtn/AddWaterBtn";
+import Logo from "../../HomePage/WelcomeSection/Logo/Logo";
+import WaterProgressBar from "./WaterProgressBar/WaterProgressBar.jsx";
 
 const WaterMainInfo = () => {
   return (
@@ -20,7 +19,7 @@ const WaterMainInfo = () => {
           className={css.image}
           src={bottleDescX1}
           srcSet={`${bottleDescX1} 1x, ${bottleDescX2} 2x`}
-          alt="Water botle image"
+          alt="Water botel image"
         />
       </div>
     </div>
--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -1,5 +1,5 @@
-import WaterMainInfo from "../../components/TrackerPage/WaterMainInfo/WaterMainInfo.jsx";
-import WaterDetailedInfo from "../../components/TrackerPage/WaterDailedInfo/WaterDetailedInfo.jsx";
+import WaterDetailedInfo from "../../components/TrackerPage/WaterDailedInfo/WaterDetailedInfo";
+import WaterMainInfo from "../../components/TrackerPage/WaterMainInfo/WaterMainInfo";
 import s from "./TrackerPage.module.css";
 
 const TrackerPage = () => {
