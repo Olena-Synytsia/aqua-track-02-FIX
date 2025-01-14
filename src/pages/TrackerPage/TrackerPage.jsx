dbe10d89f2f8931a7754db8805f2f66a00c09438
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2025-01-14 17:12:53 +0200

    Update TrackerPage.jsx

--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -1,5 +1,5 @@
 import WaterMainInfo from "../../components/TrackerPage/WaterMainInfo/WaterMainInfo.jsx";
-import WaterDetailedInfo from "../../components/TrackerPage/WaterDetailedInfo/WaterDetailedInfo.jsx";
+import WaterDetailedInfo from "../../components/TrackerPage/WaterDailedInfo/WaterDetailedInfo.jsx";
 import s from "./TrackerPage.module.css";
 
 const TrackerPage = () => {
