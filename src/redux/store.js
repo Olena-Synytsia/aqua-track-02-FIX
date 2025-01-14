a56aebb0615874e430871e4ea7fcb347615940a9
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2025-01-14 17:08:40 +0200

    avatar-select

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "./auth/slice.js";
 import { waterDeleteModal } from "./water-delete/slice.js";
+import { avatarReducer } from "./avatar/slice.js";
 
 export const store = configureStore({
   reducer: {
     user: authReducer,
     water: waterDeleteModal,
+    image: avatarReducer,
   },
 });
