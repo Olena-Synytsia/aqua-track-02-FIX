68e439a1e84804f4df392f967277303248387282
Olena-Synytsia <alenasinitsa85@gmail.com>
2025-01-16 09:00:15 +0200

    fix css SignUpForm

--- src/components/SignUpPage/SignUpForm.jsx
+++ src/components/SignUpPage/SignUpForm.jsx
@@ -58,126 +58,124 @@ const SignUpForm = () => {
   };
 
   return (
-    <div className={s.section}>
-      <div className={s.container}>
-        <div className={s.logo}>
-          <Logo />
-        </div>
-        <div className={s.form}>
-          <h2 className={s.titleSingUp}>Sign Up</h2>
-          <form onSubmit={handleSubmit(onSubmit)} className={s.formBody}>
-            <div className={s.wrapForm}>
-              <div className={s.formEl}>
-                <label className={s.label}>
-                  <span className={s.span}>Email</span>
-                </label>
-                <div className={s.inputWrap}>
-                  <input
-                    type="email"
-                    placeholder="Enter your email"
-                    className={`${s.input} ${errors.email ? s.error : ""}`}
-                    {...hookFormRegister("email")}
-                  />
-                </div>
-
-                {errors.email && (
-                  <div className={s.textError}>{errors.email.message}</div>
-                )}
+    <div className={s.container}>
+      <div className={s.logo}>
+        <Logo />
+      </div>
+      <div className={s.form}>
+        <h2 className={s.titleSingUp}>Sign Up</h2>
+        <form onSubmit={handleSubmit(onSubmit)} className={s.formBody}>
+          <div className={s.wrapForm}>
+            <div className={s.formEl}>
+              <label className={s.label}>
+                <span className={s.span}>Email</span>
+              </label>
+              <div className={s.inputWrap}>
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  className={`${s.input} ${errors.email ? s.error : ""}`}
+                  {...hookFormRegister("email")}
+                />
               </div>
 
-              <div className={s.formEl}>
-                <label className={s.label}>
-                  <span className={s.span}>Password</span>
-                </label>
-                <div className={s.inputWrap}>
-                  <input
-                    type={passwordVisible ? "text" : "password"}
-                    placeholder="Enter your password"
-                    className={`${s.input} ${errors.password ? s.error : ""}`}
-                    {...hookFormRegister("password")}
-                  />
-                  <label className={s.labelIcons}>
-                    <span
-                      className={s.spanIcons}
-                      onClick={() => setPasswordVisible((prev) => !prev)}
-                    >
-                      <svg
-                        className={s.svgSingUp}
-                        width="20"
-                        height="20"
-                        viewBox="0 0 32 32"
-                      >
-                        <use
-                          href={`/src/assets/sprite.svg#${
-                            passwordVisible ? "icon-eye" : "icon-eye-off"
-                          }`}
-                        ></use>
-                      </svg>
-                    </span>
-                  </label>
-                </div>
-
-                {errors.password && (
-                  <div className={s.textError}>{errors.password.message}</div>
-                )}
-              </div>
-
-              <div className={s.formEl}>
-                <label className={s.label}>
-                  <span className={s.span}>Repeat password</span>
-                </label>
-                <div className={s.inputWrap}>
-                  <input
-                    type={repeatPasswordVisible ? "text" : "password"}
-                    placeholder="Repeat password"
-                    className={`${s.input} ${
-                      errors.repeatPassword ? s.error : ""
-                    }`}
-                    {...hookFormRegister("repeatPassword")}
-                  />
-                  <label className={s.labelIcons}>
-                    <span
-                      className={s.spanIcons}
-                      onClick={() => setRepeatPasswordVisible((prev) => !prev)}
-                    >
-                      <svg
-                        className={s.svgSingUp}
-                        width="20"
-                        height="20"
-                        viewBox="0 0 32 32"
-                      >
-                        <use
-                          href={`/src/assets/sprite.svg#${
-                            repeatPasswordVisible ? "icon-eye" : "icon-eye-off"
-                          }`}
-                        ></use>
-                      </svg>
-                    </span>
-                  </label>
-                </div>
-
-                {errors.repeatPassword && (
-                  <div className={s.textError}>
-                    {errors.repeatPassword.message}
-                  </div>
-                )}
-              </div>
+              {errors.email && (
+                <div className={s.textError}>{errors.email.message}</div>
+              )}
             </div>
 
-            <div className={s.divBtn}>
-              <button type="submit" className={s.singUpBtn}>
-                Sign Up
-              </button>
-              <p className={s.text}>
-                Already have an account?
-                <Link to="/signin" className={s.link}>
-                  <span className={s.spanLink}>Sign In</span>
-                </Link>
-              </p>
+            <div className={s.formEl}>
+              <label className={s.label}>
+                <span className={s.span}>Password</span>
+              </label>
+              <div className={s.inputWrap}>
+                <input
+                  type={passwordVisible ? "text" : "password"}
+                  placeholder="Enter your password"
+                  className={`${s.input} ${errors.password ? s.error : ""}`}
+                  {...hookFormRegister("password")}
+                />
+                <label className={s.labelIcons}>
+                  <span
+                    className={s.spanIcons}
+                    onClick={() => setPasswordVisible((prev) => !prev)}
+                  >
+                    <svg
+                      className={s.svgSingUp}
+                      width="20"
+                      height="20"
+                      viewBox="0 0 32 32"
+                    >
+                      <use
+                        href={`/src/assets/sprite.svg#${
+                          passwordVisible ? "icon-eye" : "icon-eye-off"
+                        }`}
+                      ></use>
+                    </svg>
+                  </span>
+                </label>
+              </div>
+
+              {errors.password && (
+                <div className={s.textError}>{errors.password.message}</div>
+              )}
             </div>
-          </form>
-          {notification && <div className={s.notification}>{notification}</div>}
-        </div>
+
+            <div className={s.formEl}>
+              <label className={s.label}>
+                <span className={s.span}>Repeat password</span>
+              </label>
+              <div className={s.inputWrap}>
+                <input
+                  type={repeatPasswordVisible ? "text" : "password"}
+                  placeholder="Repeat password"
+                  className={`${s.input} ${
+                    errors.repeatPassword ? s.error : ""
+                  }`}
+                  {...hookFormRegister("repeatPassword")}
+                />
+                <label className={s.labelIcons}>
+                  <span
+                    className={s.spanIcons}
+                    onClick={() => setRepeatPasswordVisible((prev) => !prev)}
+                  >
+                    <svg
+                      className={s.svgSingUp}
+                      width="20"
+                      height="20"
+                      viewBox="0 0 32 32"
+                    >
+                      <use
+                        href={`/src/assets/sprite.svg#${
+                          repeatPasswordVisible ? "icon-eye" : "icon-eye-off"
+                        }`}
+                      ></use>
+                    </svg>
+                  </span>
+                </label>
+              </div>
+
+              {errors.repeatPassword && (
+                <div className={s.textError}>
+                  {errors.repeatPassword.message}
+                </div>
+              )}
+            </div>
+          </div>
+
+          <div className={s.divBtn}>
+            <button type="submit" className={s.singUpBtn}>
+              Sign Up
+            </button>
+            <p className={s.text}>
+              Already have an account?
+              <Link to="/signin" className={s.link}>
+                <span className={s.spanLink}>Sign In</span>
+              </Link>
+            </p>
+          </div>
+        </form>
+        {notification && <div className={s.notification}>{notification}</div>}
       </div>
     </div>
   );
