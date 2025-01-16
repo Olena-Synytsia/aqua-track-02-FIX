e36bf0086ee385f40ae226ba83f6ffe63c79479c
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2025-01-16 19:40:27 +0200

    update

--- src/components/TrackerPage/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/TrackerPage/WaterMainInfo/WaterMainInfo.jsx
@@ -1,30 +1,24 @@
 import AddWaterB from "./AddWaterBtn/AddWaterB.jsx";
 import WaterDailyNorma from "./WaterDailyNorma/WaterDailyNorma.jsx";
 import WaterProgressBar from "./WaterProgressBar/WaterProgressBar.jsx";
-// import bottleDescX1 from "../../../assets/img/bottel@x1.webp";
-// import bottleDescX2 from "../../../assets/img/bottel@x2.webp";
 import Logo from "../../HomePage/WelcomeSection/Logo/Logo";
 import css from "./WaterMainInfo.module.css";
-
 const WaterMainInfo = () => {
   return (
     <div className={css.container}>
-      <div className={css.logoTracker}>
+      <div className={css.logo}>
         <Logo />
       </div>
-      <WaterDailyNorma />
-      <WaterProgressBar />
-      <AddWaterB />
-      {/* <div className={css.imageContainer}>
-        <img
-          className={css.image}
-          src={bottleDescX1}
-          srcSet={`${bottleDescX1} 1x, ${bottleDescX2} 2x`}
-          alt="Water botel image"
-        />
-      </div> */}
+      <div className={css.dailyNorma}>
+        <WaterDailyNorma />
+      </div>
+      <div className={css.progressBar}>
+        <WaterProgressBar />
+      </div>
+      <div className={css.addWater}>
+        <AddWaterB />
+      </div>
     </div>
   );
 };
-
 export default WaterMainInfo;
