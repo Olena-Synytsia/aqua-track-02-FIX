b655ad5c3e02dccbda6b6f3086fbe71f05ecfdde
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2025-01-17 16:27:14 +0200

    update

--- src/components/TrackerPage/WaterMainInfo/AddWaterBtn/AddWaterB.jsx
+++ src/components/TrackerPage/WaterMainInfo/AddWaterBtn/AddWaterB.jsx
@@ -3,6 +3,7 @@ import WaterModal from "../../../Modal/WaterModal/WaterModal.jsx";
 import css from "./AddWaterB.module.css";
 import { useDispatch } from "react-redux";
 import { setOperationType } from "../../../../redux/dailyInfo/dailyInfoSlice.js";
+
 const AddWaterB = () => {
   const dispatch = useDispatch();
   const [isOpen, setIsOpen] = useState(false);
@@ -10,6 +11,7 @@ const AddWaterB = () => {
     setIsOpen(true);
     dispatch(setOperationType("add"));
   };
+
   return (
     <>
       <button onClick={handleAdd} className={css.addButton}>
