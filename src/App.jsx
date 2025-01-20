6955a9f91a24af7aa0dfecefb9960352b5e147a4
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2025-01-20 19:18:13 +0200

    appp

--- src/App.jsx
+++ src/App.jsx
@@ -14,17 +14,21 @@ import { setToken } from "./redux/auth/slice";
 import { Suspense, lazy } from "react";
 import Loader from "./components/HomePage/WelcomeSection/Loader/Loader.jsx";
 import "./App.css";
+
 const SharedLayout = lazy(() => import("./components/SharedLayout.jsx"));
 const WelcomePage = lazy(() => import("./pages/WelcomePage/WelcomePage.jsx"));
 const RegisterPage = lazy(() =>
   import("./pages/RegisterPage/RegisterPage.jsx")
 );
+
 const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage.jsx"));
 const TrackerPage = lazy(() => import("./pages/TrackerPage/TrackerPage.jsx"));
+
 function App() {
   const dispatch = useDispatch();
   const token = useSelector(selectTokens); // Дістаємо токен із Redux
   const isRefreshing = useSelector(selectIsRefreshing); // Стан оновлення користувача
+
   useEffect(() => {
     // Перевіряємо токен у localStorage
     const localToken = localStorage.getItem("accessToken");
@@ -32,10 +36,29 @@ function App() {
       dispatch(setToken({ accessToken: localToken })); // Зберігаємо токен у Redux
     }
   }, [dispatch]);
+
   useEffect(() => {
+    const refreshToken = async () => {
+      try {
+        if (token) {
+          // Виконуємо запит на оновлення токена, якщо є токен
+          const response = await dispatch(refresh());
+
+          if (response?.status === 401) {
+            // Якщо отримали 401, то можна спробувати оновити токен
+            // Ваш механізм для отримання нового токена
+          }
+        }
+      } catch (error) {
+        if (error.response?.status === 401) {
+          // Якщо сервер відповідає статусом 401, пробуємо оновити токен
+          dispatch(refresh()); // Запит на оновлення
+        }
+      }
+    };
+
     if (token) {
-      // Якщо токен є, оновлюємо користувача
-      dispatch(refresh());
+      refreshToken();
     }
   }, [dispatch, token]);
   // Відображаємо завантаження, якщо refresh() ще працює
