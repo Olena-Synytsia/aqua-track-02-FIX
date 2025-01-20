aa1aa2e19249a7189a6e8801cae752d364f3c2ca
Olga Goiko <olga.a10@ukr.net>
2025-01-20 14:32:03 +0200

    add changes in CalendarItem

--- src/components/TrackerPage/WaterDailedInfo/MonthInfo/Calendar/CalendarItem.jsx
+++ src/components/TrackerPage/WaterDailedInfo/MonthInfo/Calendar/CalendarItem.jsx
@@ -1,18 +1,30 @@
 import s from "./CalendarItem.module.css";
 import clsx from "clsx";
 import dayjs from "dayjs";
+import { useDispatch } from "react-redux";
+import {setWaterDay} from "../../../../../redux/dailyInfo/dailyInfoSlice"
 
-const getFormattedDate = (selectedDate, day) => {
-  const date = new Date(selectedDate);
-  date.setDate(day);
-  return dayjs(date).format("YYYY-MM-DD");
-};
+// const getFormattedDate = (selectedDate, day) => {
+//   const date = new Date(selectedDate);
+//   date.setDate(day);
+//   return dayjs(date).format("YYYY-MM-DD");
+// };
 
 const CalendarItem = ({ day, selectedDate, onDateSelect, percent }) => {
-  const handleClick = () => {
-    const formattedDate = getFormattedDate(selectedDate, day);
-    onDateSelect(formattedDate);
-  };
+  const dispatch = useDispatch();
+
+    const getFormattedDate = (selectedDate, day) => {
+      const date = new Date(selectedDate);
+      date.setDate(day);
+      const waterDay = date.toISOString().split("T")[0];
+      dispatch(setWaterDay(waterDay));
+      return dayjs(date).format("YYYY-MM");  
+};
+
+const handleClick = () => {
+  const formattedDate = getFormattedDate(selectedDate, day);
+  onDateSelect(formattedDate);
+};
 
   const isSelected = dayjs(selectedDate).date() === day;
 
