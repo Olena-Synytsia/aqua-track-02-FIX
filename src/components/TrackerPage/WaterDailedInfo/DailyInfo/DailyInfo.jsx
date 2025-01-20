ad3cf665819ad1111b67fc4d973ad477f6b114d6
Roman Zhelnov <roman95zhelnov@gmail.com>
2025-01-20 14:20:30 +0200

    add fix water day

--- src/components/TrackerPage/WaterDailedInfo/DailyInfo/DailyInfo.jsx
+++ src/components/TrackerPage/WaterDailedInfo/DailyInfo/DailyInfo.jsx
@@ -1,21 +1,19 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import AddWaterBtn from "./AddWaterBtn/AddWaterBtn.jsx";
 import ChooseDate from "./ChooseDate/ChooseDate.jsx";
 import s from "./DailyInfo.module.css";
 import WaterList from "./WaterList/WaterList.jsx";
 import { useEffect } from "react";
 import { fetchWaterItem } from "../../../../redux/dailyInfo/dailyInfoOps.js";
-// import { useSelect } from "@chakra-ui/react";
-// import { selectDate } from "../../../../redux/water/selectors.js";
+import { selectWaterDay } from "../../../../redux/dailyInfo/dailyInfoSlice.js";
 
 const DailyInfo = () => {
-  // const day = useSelect(selectDate);
-  const date = "2025-01-19";
+  const waterDay = useSelector(selectWaterDay);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchWaterItem(date));
-  }, [date, dispatch]);
+    dispatch(fetchWaterItem(waterDay));
+  }, [waterDay, dispatch]);
   return (
     <div className={s.dailyInfo}>
       <div className={s.textAndBtn}>
