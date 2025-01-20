318593f73999cbb43157b5f85e339721f2610c3f
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2025-01-20 21:12:13 +0200

    svggggg

--- src/components/TrackerPage/WaterDailedInfo/UserPanel/UserBar/UserBarBtn/UserBarBtn.jsx
+++ src/components/TrackerPage/WaterDailedInfo/UserPanel/UserBar/UserBarBtn/UserBarBtn.jsx
@@ -86,7 +86,7 @@ const UserBarBtn = ({ userName, avatarUrl }) => {
               transition: "transform 0.3s ease",
             }}
           >
-            <use href="../../../../../../assets/sprite.svg#icon-chevron-down-1" />
+            <use href="/../../../../../assets/sprite.svg#icon-chevron-down-1" />
           </svg>
         </button>
       </div>
